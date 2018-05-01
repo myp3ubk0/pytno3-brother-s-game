6f7c635ca1fa558c43924028b4ab3c2b8496c343
myp3ubk0 <myp3ubk0@gmail.com>
2018-05-01 10:14:49 +0500

    конфиг клавиш в отдельном скрипте, ожидание следующего выпуска яблок - 10 циклов

---
+++ keys.py
@@ -0,0 +1,7 @@
+keysDictionary = {
+    "keyUp" : "Up",
+    "keyDown" : "Down",
+    "keyLeft" : "Left",
+    "keyRight" : "Right",
+    "keyAttack" : "x"
+}
--- objects.py
+++ objects.py
@@ -2,6 +2,7 @@ from screen import *
 from C_NPC import *
 from C_ITEM import *
 from random import *
+from keys import keysDictionary
 
 root = getScreenRoot()
 npcCollection = {}
@@ -19,14 +20,13 @@ def addItem(item):
     itemCollection[itemCollectionIndex] = item
     itemCollectionIndex = itemCollectionIndex + 1
 
-addNpc(C_NPC("player",5)) # обязательно должен быть первым!!
 addNpc(C_ENEMY("copy",4))
 addNpc(C_ENEMY("wolf",4))
 addNpc(C_ENEMY("copy",4))
 addNpc(C_ENEMY("copy",4))
 addNpc(C_ENEMY("copy",4))
 addNpc(C_ENEMY("wolf",4))
-addItem(C_ITEM("bullet",5,False,300,300))
+addNpc(C_NPC("player",5))
 
 def initAllObjects():
     for i in range(0, len(npcCollection)):
@@ -75,7 +75,7 @@ def checkCollision():
 def playerHit(id = -1):
     if (id == -1):
         for i in range(0, len (npcCollection)):
-            if (npcCollection[i].name == "player") : id = i
+            if ((not (npcCollection[i] is None)) and (npcCollection[i].name == "player")) : id = i
     npcCollection[id].health = npcCollection[id].health - 1
     if (npcCollection[id].health <= 0):
         npcCollection[id].screenObject.destroy()
@@ -95,32 +95,30 @@ def _released(event):
 
 def playerMove(press):
     x=0;y=0
-    if (press["Up"] and not press["Down"]): y=-1
-    if (press["Down"] and not press["Up"]): y=1
-    if (press["Left"] and not press["Right"]): x=-1
-    if (press["Right"] and not press["Left"]): x=1
+    if (press[keysDictionary["keyUp"]] and not press[keysDictionary["keyDown"]]): y=-1
+    if (press[keysDictionary["keyDown"]] and not press[keysDictionary["keyUp"]]): y=1
+    if (press[keysDictionary["keyLeft"]] and not press[keysDictionary["keyRight"]]): x=-1
+    if (press[keysDictionary["keyRight"]] and not press[keysDictionary["keyLeft"]]): x=1
     findPlayer().playerGo(x,y)
 
 def playerShoot(press):
     player = findPlayer()
     if (player.attack > 0):
-        player.attack -=1
-        vx=0
-        vy=0
-        if ((press["Up"] and not press["Down"]) or (player.spriteStand == player.spriteUpStand)): vy=-1
-        if ((press["Down"] and not press["Up"]) or (player.spriteStand == player.spriteDownStand)): vy=1
-        if ((press["Left"] and not press["Right"]) or (player.spriteStand == player.spriteLeftStand)): vx=-1
-        if ((press["Right"] and not press["Left"]) or (player.spriteStand == player.spriteRightStand)): vx=1
+        player.attack -=1; vx=0; vy=0
+        if ((press[keysDictionary["keyUp"]] and not press[keysDictionary["keyDown"]]) or (player.spriteStand == player.spriteUpStand)): vy=-1
+        if ((press[keysDictionary["keyDown"]] and not press[keysDictionary["keyUp"]]) or (player.spriteStand == player.spriteDownStand)): vy=1
+        if ((press[keysDictionary["keyLeft"]] and not press[keysDictionary["keyRight"]]) or (player.spriteStand == player.spriteLeftStand)): vx=-1
+        if ((press[keysDictionary["keyRight"]] and not press[keysDictionary["keyLeft"]]) or (player.spriteStand == player.spriteRightStand)): vx=1
         addItem(C_BULLET("bullet",5,True,player.posX,player.posY,vx,vy))
         
-
 timeToCreate = 0
+timeToShoot = 0
 def _animate():
-    global timeToCreate
-    if (pressed["Up"] or pressed["Down"] or pressed["Left"] or pressed["Right"]): 
+    global timeToCreate, timeToShoot
+    if (pressed[keysDictionary["keyUp"]] or pressed[keysDictionary["keyDown"]] or pressed[keysDictionary["keyLeft"]] or pressed[keysDictionary["keyRight"]]): 
         if not (findPlayer() is None): playerMove(pressed)
-    if (pressed["space"]): 
-        if not (findPlayer() is None): playerShoot(pressed)
+    if (pressed[keysDictionary["keyAttack"]]): 
+        if ((not (findPlayer() is None)) and (timeToShoot == 0)): playerShoot(pressed)
     for i in range(0, len (itemCollection)):
         if (not (itemCollection[i] is None)):
             if (itemCollection[i].firing):
@@ -129,15 +127,18 @@ def _animate():
                     itemCollection[i].screenObject.destroy()
                     itemCollection[i] = None
     checkCollision()
-    timeToCreate = timeToCreate + 1
     if (timeToCreate >= 100):
         timeToCreate = 0
         addItem(C_ITEM("bullet",5,False,randint(0,root.winfo_screenwidth()),randint(0,root.winfo_screenheight())))
+    else: timeToCreate += 1
+    if (timeToShoot >=10 and timeToShoot != 0):
+        timeToShoot = 0
+    else: timeToShoot += 1
     weaponLabel.configure(text = "You DEAD" if (findPlayer() is None) else findPlayer().attack)
     weaponLabel.update()
     root.after(15, _animate)
     
-for char in ["Up","Down","Left", "Right", "space"]:
+for char in [keysDictionary["keyUp"],keysDictionary["keyDown"],keysDictionary["keyLeft"], keysDictionary["keyRight"], keysDictionary["keyAttack"]]:
     root.bind("<KeyPress>", _pressed)
     root.bind("<KeyRelease>", _released)
     pressed[char] = False
